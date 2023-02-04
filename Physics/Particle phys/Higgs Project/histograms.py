a15f28b915146bbbde924cafafbff376ad224ec7
Dawid Zapolski <dzapolski3@gmail.com>
2023-02-04 09:15:59 +0000

    Histogram of MET, 2 leptons, qqZH

---
+++ Physics/Particle phys/Higgs Project/histograms.py
@@ -0,0 +1,44 @@
+import uproot
+import matplotlib.pyplot as plt
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# def create_histogram(branch):
+#     ggZZ = ("qqZllH125", "")
+
+
+
+def main():
+    logger.info("Opening file")
+    file_location = "/home/daw/Documents/Physics/Particle phys/Higgs Project/"
+
+    file_name = {'lepton0': "0leptons.root",
+                'lepton1': "1lepton.root",
+                'lepton2': "2leptons.root"}
+
+    # channels = ("lepton0", "lepton2")
+    channels = "lepton2"
+    # dataframe_path = "/home/daw/Documents/Physics/Particle phys/Higgs Project/"
+
+    branches = {}
+    # dataframes = {}
+    for channel in channels:
+        logger.info('Opening' + file_name[channel])
+        branches[channel] = uproot.open(file_location + file_name[channel] + ':Nominal')
+        # print(branches[channel].show())
+        logger.info('Opening finished')
+        qqZZ_mask = branches[channel]['Sample'] == "qqZllH125"
+        histogram = plt.hist(branches[channel]['MET'][qqZZ_mask])
+        plt.xlabel("MET")
+        plt.show()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
