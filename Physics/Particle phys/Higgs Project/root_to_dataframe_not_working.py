9e0f45bd1427d5d1c5ff0fbe2658888830087312
Dawid Zapolski <dzapolski3@gmail.com>
2023-02-08 01:13:10 +0000

    Convert to pandas while reading only variables of interest. Currently working only for lepton2

--- Physics/Particle phys/Higgs Project/root_to_dataframe_not_working.py
+++ Physics/Particle phys/Higgs Project/root_to_dataframe_not_working.py
@@ -5,36 +5,39 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def main():
+def read_and_pickle(channels=None):
+    """Possible inputs are ["lepton0","lepton2"]"""
+    if channels is None:
+        channels = ["lepton2"]
+
+
     file_location = "/home/daw/Documents/Physics/Particle phys/Higgs Project/"
 
     file_name = {'lepton0': "0leptons.root",
                 'lepton1': "1lepton.root",
                 'lepton2': "2leptons.root"}
 
-    # channels = ("lepton0", "lepton2")
-    channels = ('lepton0','')
-    dataframe_path = "/home/daw/Documents/Physics/Particle phys/Higgs Project/"
 
-    trees = {}
-    dataframes = {}
+    dataframe_path = "/home/daw/Documents/Physics/Particle phys/Higgs Project/"
+    variables_of_interest = ["pTVH","pTV","ptL1","ptL2",'pTBBJ', 'pTBB', 'nJets', 'mLB','HT',"GSCMvh","etaL1","dRBB", "dPhiLL", "dPhiBBcor", "dEtaVBB"]
+    additional_info = ["EventWeight", "Sample"]
+
     for channel in channels:
         logger.info('Opening' + file_name[channel])
-        trees[channel] = uproot.open(file_location + file_name[channel] + ':Nominal')
-        print(trees[channel].show())
+        branches = uproot.open(file_location + file_name[channel] + ':Nominal')
+        print(branches.show())
         logger.info('Openieng finished')
 
-
         logger.info("Converting to pandas dataframe")
-        dataframes[channel] = trees[channel].arrays(library='pd')
+        dataframes = branches.arrays(variables_of_interest + additional_info,library='pd')
         logger.info("Converting finished")
-        print(channel + " data frame\n",dataframes[channel].head())
+        print(channel + " data frame\n",dataframes.head())
 
         logger.info("Pickling to file")
-        dataframes[channel].to_pickle(dataframe_path + channel + ".pkl")
+        dataframes.to_pickle(dataframe_path + channel + "VOI.pkl")
 
 
 
 
 if __name__ == "__main__":
-    main()
+    read_and_pickle()
