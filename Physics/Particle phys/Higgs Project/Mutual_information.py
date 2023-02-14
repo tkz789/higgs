29df1eeb0034ccfff15066b2f8941d9ce19c0816
Dawid Zapolski <dzapolski3@gmail.com>
2023-02-14 23:00:48 +0000

    Repaired lack of "Sample" variable

--- Physics/Particle phys/Higgs Project/Mutual_information.py
+++ Physics/Particle phys/Higgs Project/Mutual_information.py
@@ -1,17 +1,19 @@
-import pandas as pd
-import numpy as np
 import logging
+
+import numpy as np
+import pandas as pd
 from sklearn.feature_selection import mutual_info_classif
-from sklearn.feature_selection import VarianceThreshold
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 def make_mi_scores(X, y, discrete_features):
     mi_scores = mutual_info_classif(X, y, discrete_features=discrete_features)
     mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
     mi_scores = mi_scores.sort_values(ascending=False)
     return mi_scores
+
 
 def main():
     file_location = "/home/daw/Documents/Physics/Particle phys/Higgs Project/"
@@ -21,28 +23,29 @@ def main():
     for channel in channels:
         logger.info('Opening')
 
-
         df = pd.read_pickle("lepton2whole_qq_gg")
         logger.info("Opening finished")
         print(df.head())
-        selection = VarianceThreshold()
-        df = selection.fit_transform(df)
-        logger.info("Transformation finished")
-        print(df.head())
-
+        # selection = VarianceThreshold()
+        # df = selection.fit_transform(df)
+        # logger.info("Transformation finished")
+        # df = pd.DataFrame(df, )
+        # print(df.head())
 
         df['is_ggZH'] = np.logical_or(df["Sample"] == "ggZllH125", df["Sample"] == "ggZllH125cc")
         logger.info("Selection made")
-        print(channel + " data frame\n",df.head())
+        print(channel + " data frame\n", df.head())
         logger.info("Mutual_information")
         keys = df.columns.values
-        keys.remove("Sample")
-        keys.remove("is_ggZH")
+        keys = np.delete(keys, 0)
+        keys = np.delete(keys, -1)
+        print(keys)
         discrete_features = df[keys].dtypes == int
         mi_score = make_mi_scores(df[keys], df['is_ggZH'], discrete_features)
         logger.info("Pickling")
-        mi_score.to_pickle("MIscore.pcl")
+        mi_score.to_pickle("MIscore.pkl")
         print(mi_score)
+
 
 if __name__ == "__main__":
     main()
--- Physics/Particle phys/Higgs Project/root_into_dataframe_dask.py
+++ Physics/Particle phys/Higgs Project/root_into_dataframe_dask.py
@@ -18,12 +18,20 @@ def main():
         branches = uproot.open(file_name[channel] + ":Nominal")
         logger.info("Finished")
         df = pd.DataFrame()
+        df["Sample"] = awkward.to_dataframe(branches.arrays(["Sample"]))
+        df = df.loc[(df["Sample"] == "qqZllH125") |
+                    (df["Sample"] == "ggZllH125") |
+                    (df["Sample"] == "ggZllH125cc") |
+                    (df["Sample"] == "qqZllH125cc")]
         for key in branches.keys():
             if branches.typenames()[key] in ["int32_t", "float"]:
                 logger.info("Converting" + key)
-                selected_array = awkward.to_dataframe(branches.arrays([key,"Sample"]))
+                selected_array = awkward.to_dataframe(branches.arrays([key, "Sample"]))
                 print(selected_array.head())
-                selected_array = selected_array.loc[(selected_array["Sample"] == "qqZllH125")|(selected_array["Sample"] == "ggZllH125")|(selected_array["Sample"] == "ggZllH125cc")|(selected_array["Sample"] == "qqZllH125cc")]
+                selected_array = selected_array.loc[(selected_array["Sample"] == "qqZllH125") |
+                                                    (selected_array["Sample"] == "ggZllH125") |
+                                                    (selected_array["Sample"] == "ggZllH125cc") |
+                                                    (selected_array["Sample"] == "qqZllH125cc")]
                 df[key] = selected_array[key]
         print(df.head())
         logger.info("Pickling")
